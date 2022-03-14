c1927237013d2ab3d3fd9fa2c844915f6d4816fc
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-14 15:52:46 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_OOP_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_OOP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player vasiliy = new Player("Васян", 100, 5);
+
+            vasiliy.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        public string Name;
+        public int Health;
+        public int Damage;
+
+        public Player(string name, int health, int damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Имя игрока - " + Name + "\nЕго жизни - " + Health + "\nЕго урон - " + Damage);
+        }
+    }
+}
